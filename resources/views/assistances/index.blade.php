649f510223cd370f410beabc76ac396fc0f36e9b
ivanng01 <ivann-gonzalez@hotmail.com>
2024-09-25 14:24:05 -0300

    Cambios en boton assistances

--- resources/views/assistances/index.blade.php
+++ resources/views/assistances/index.blade.php
@@ -115,7 +115,7 @@
                     </table>
                     
                     <div class="flex justify-end mt-6">
-                        <button type="submit" class="text-white bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar Asistencia</button>
+                        <button type="submit" class="text-white bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar Asistencia</button>
                     </div>
                 </form>
             </div>
